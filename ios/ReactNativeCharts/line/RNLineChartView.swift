a9e3e0ec889eabe48117cf6b146df40b8e00fd2d
Charlie von Lehe <vonlehec@gmail.com>
2019-09-25 14:51:47 -0400

    disabled marker and highlight on ios

--- ios/ReactNativeCharts/line/RNLineChartView.swift
+++ ios/ReactNativeCharts/line/RNLineChartView.swift
@@ -25,10 +25,23 @@ class RNLineChartView: RNBarLineChartViewBase {
         super.init(frame: frame);
         
         self._chart.delegate = self
+        let recognizer = UIPanGestureRecognizer(target: self, action: #selector(handlePan(_:)))
+        recognizer.delegate = self
+        self._chart.addGestureRecognizer(recognizer)
         self.addSubview(_chart);
         
     }
     
+    func handlePan(_ recognizer: UIPanGestureRecognizer) {
+        self.chart.drawMarkers = recognizer.state != .ended
+        self.chart.data?.highlightEnabled = recognizer.state != .ended
+        self.chart.notifyDataSetChanged()
+    }
+
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+    }
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
