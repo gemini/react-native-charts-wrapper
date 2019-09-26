202c93e2b1f482dd92dce8351929507fcbfd2557
Charlie von Lehe <vonlehec@gmail.com>
2019-09-26 11:08:53 -0400

    removed chart highlight from pan on ios

--- ios/ReactNativeCharts/line/RNLineChartView.swift
+++ ios/ReactNativeCharts/line/RNLineChartView.swift
@@ -8,41 +8,39 @@ import SwiftyJSON
 class RNLineChartView: RNBarLineChartViewBase, UIGestureRecognizerDelegate {
     let _chart: LineChartView;
     let _dataExtract : LineDataExtract;
-    
+
     override var chart: LineChartView {
         return _chart
     }
-    
+
     override var dataExtract: DataExtract {
         return _dataExtract
     }
-    
+
     override init(frame: CoreGraphics.CGRect) {
-        
+
         self._chart = LineChartView(frame: frame)
         self._dataExtract = LineDataExtract()
-        
+
         super.init(frame: frame);
-        
+
         self._chart.delegate = self
         let recognizer = UIPanGestureRecognizer(target: self, action: #selector(handlePan(_:)))
         recognizer.delegate = self
         self._chart.addGestureRecognizer(recognizer)
         self.addSubview(_chart);
-        
+
     }
-    
+
     func handlePan(_ recognizer: UIPanGestureRecognizer) {
         if (recognizer.state == .began) {
             self.chart.drawMarkers = true
-            self.chart.data?.highlightEnabled = true
             if (self.onPanStart != nil) {
                 self.onPanStart!(nil)
              }
             self.chart.notifyDataSetChanged()
         }else if (recognizer.state == .ended) {
-            self.chart.drawMarkers = recognizer.state != .ended
-            self.chart.data?.highlightEnabled = recognizer.state != .ended
+            self.chart.drawMarkers = false
             if (self.onPanEnd != nil) {
                 self.onPanEnd!(nil)
             }
@@ -58,5 +56,5 @@ class RNLineChartView: RNBarLineChartViewBase, UIGestureRecognizerDelegate {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
 }
